33e719184c8c8dc400301f9d0427cc050a7ec665
Angus Helm <angus.helm@slalom.com>
2020-06-08 15:38:08 -0500

    try code instead of courseDAta

--- session-02-Angus/customize_env.sh
+++ session-02-Angus/customize_env.sh
@@ -1,11 +1,3 @@
-#!/bin/bash
-
-cat >> /root/.bashrc <<HEREDOC
-
 function exit() {
     echo "Please don't exit the root Katacoda session, you'll lose all your work"
 }
-
-HEREDOC
-
-source /root/.bashrc
